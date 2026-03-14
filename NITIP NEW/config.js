fb721b70b2bc4b0ad54af7e6f2a04069ce2d7975
ramastkiph-beep <rama.stkiph@gmail.com>
2026-03-14 10:24:17 +0700

    Add files via upload

---
+++ NITIP NEW/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL  = 'https://YOUR_PROJECT.supabase.co';
+const SUPABASE_ANON = 'YOUR_ANON_KEY';
+const APP_NAME      = 'Nitip Tuban';
+const APP_TAGLINE   = 'Jasa Titip & Antar Tuban';
