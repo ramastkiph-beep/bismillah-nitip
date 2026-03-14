59693899408e04ba82b7adb8de0344839b1ded75
ramastkiph-beep <rama.stkiph@gmail.com>
2026-03-14 10:39:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL  = 'https://ydvddprjwbqfhbukswnk.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkdmRkcHJqd2JxZmhidWtzd25rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MDMzMDAsImV4cCI6MjA4ODk3OTMwMH0.Ul5kT2D8GKA7u8eEMPzJENlNOtcgFlV_pIVtIOVctrs';
+const APP_NAME      = 'Nitip Tuban';
+const APP_TAGLINE   = 'Jasa Titip & Antar Tuban';
